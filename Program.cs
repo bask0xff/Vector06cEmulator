8d1ae54f8c3944067e770905081049179685295e
sergey <bask0xff@gmail.com>
2026-04-01 13:07:30 +0200

    update comments

--- Program.cs
+++ Program.cs
@@ -9,7 +9,8 @@ namespace Vector06cEmulator
             var emu = new Emulator();
 
             // Сначала мониторы, потом программа
-            emu.LoadMonitors("Monitor0.rom", "MonitorF.rom");
+            emu.LoadMonitors("Monitor0.rom", "MonitorF.rom"); // взято из /Emulator3000/"ЮТ-88"
+            //emu.LoadMonitors("Vector06C.rom", "BootTIMSoft.rom"); // из папки /Emulator3000/Vector06C
             emu.LoadRom("binland.rom");
             emu.Start();
             emu.Run(maxSteps: 10_000_000);
